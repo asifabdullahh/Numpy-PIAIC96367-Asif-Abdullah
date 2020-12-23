277a13bfe063e406a96aef6d38b5f1d4068f5a0c
asifabdullahh <asifmagh@gmail.com>
2020-12-24 02:48:31 +0500

    update

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+def function1():
+    # create 2d array from 1,12 range 
+    # dimension should be 6row 2 columns  
+    # and assign this array values in x values in x variable
+    # Hint: you can use arange and reshape numpy methods  
+    x =  np.arange(1,13).reshape((6,2)) 
+
+    return x
+    """
+    expected output:
+    [[ 1  2]
+    [ 3  4]
+    [ 5  6]
+    [ 7  8]
+    [ 9 10]
+    [11 12]]
+    """
+x = function1()
+print(x)
